6edad13137572d53da76e1f88c117b5e204fb367
Luca Yeray <lucayeray.andrade@institutvidreres.cat>
2024-05-28 12:28:14 +0200

    gg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomelucayerayComponent } from "./homelucayeray/homelucayeray.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [{ path: 'home', component: HomelucayerayComponent }];
